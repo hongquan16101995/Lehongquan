e8d36000ab04e2364827d9dc8bdb6cf8e7a5f912
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-06-08 15:02:34 +0700

    uptoantu

---
+++ toantu.js
@@ -0,0 +1,12 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt('Enter the width');
+inputheight = prompt('Enter the height');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write('Enter width: ' + inputwidth);
+document.write('<br/>');
+document.write('Enter height: ' + inputheight);
+document.write('<br/>');
+document.write('The area is: ' + area)
