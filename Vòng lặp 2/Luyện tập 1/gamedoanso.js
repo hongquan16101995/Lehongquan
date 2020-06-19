a2abb1b93023e84191977f2135a0c98d8bc6b041
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-06-19 09:23:06 +0700

    sualoiten

--- Vòng lặp 2/Luyện tập 1/gamedoanso.js
+++ Vòng lặp 2/Luyện tập 1/gamedoanso.js
@@ -1,25 +1,25 @@
 function doanso() {
     let num1 = +prompt("Nhập vào số thứ nhất: ");
     let num2 = +prompt("Nhập vào số thứ hai: ");
-    let numdoan = +prompt("Nhập vào số bạn đoán: ");
-    let numran = Math.round(Math.random()*(num2-num1)+num1);
-    while (numdoan < num1 || numdoan > num2) {
+    let guestNum = +prompt("Nhập vào số bạn đoán: ");
+    let ranNum = Math.round(Math.random()*(num2-num1)+num1);
+    while (guestNum < num1 || guestNum > num2) {
             alert("Bạn đã nhập sai, mời nhập lại!")
-            numdoan = +prompt("Nhập vào số bạn đoán: ")
+        guestNum = +prompt("Nhập vào số bạn đoán: ")
         }
-    if (numdoan!=numran){
+    if (guestNum!=ranNum){
         let i=1;
-        while (numdoan!=numran){
-            numdoan = +prompt("Nhập vào số bạn đoán: ")
+        while (guestNum!=ranNum){
+            guestNum = +prompt("Nhập vào số bạn đoán: ")
             i++
             if(i==3) {
                 alert("Bạn đã thua!")
                 break;
-            }else if(numdoan==numran){
+            }else if(guestNum==ranNum){
                 alert("Chúc mừng bạn đã đoán đúng!")
                 break;
             }
         }
     } else alert("Chúc mừng bạn đã đoán đúng!")
-    document.write("Số đúng là: "+numran)
+    document.write("Số đúng là: "+ranNum)
 }
